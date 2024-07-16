93698c59589fffa5091aa5b866aa5665fdc3d6d8
Stefan Dragoi <Stefan.Dragoi@centric.eu>
2024-07-16 21:30:54 +0300

    Partially validator test & exercise 2

--- SummerPractice.TravelPlanner/TravelPlanner.Validators.Tests/RegisterRequestModelValidatorTests.cs
+++ SummerPractice.TravelPlanner/TravelPlanner.Validators.Tests/RegisterRequestModelValidatorTests.cs
@@ -2,10 +2,53 @@ namespace TravelPlanner.Validators.Tests
 {
     using API.Validators.User;
     using Application.Models.User.Requests;
+    using FluentValidation.TestHelper;
 
     public class RegisterRequestModelValidatorTests
     {
         private readonly RegisterRequestModelValidator _validator = new();
+
+        [Fact]
+        public void Should_HaveError_When_UsernameIsEmpty()
+        {
+            // Arrange
+            var model = this.GetValidModel();
+            model.Username = string.Empty;
+
+            // Act
+            var result = this._validator.TestValidate(model);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.Username);
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Password_Does_Not_Contain_Lowercase()
+        {
+            // Arrange
+            var model = this.GetValidModel();
+            model.Password = "PASSWORD1!";
+
+            // Act
+            var result = this._validator.TestValidate(model);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.Password);
+        }
+
+        [Fact]
+        public void Should_HaveError_When_EmailIsInvalid()
+        {
+            // Arrange
+            var model = this.GetValidModel();
+            model.Email = "invalid-email";
+
+            // Act
+            var result = this._validator.TestValidate(model);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.Email);
+        }
 
         private RegisterRequestModel GetValidModel()
         {
