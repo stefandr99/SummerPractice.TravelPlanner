ca30b3f96284400e81ef9e1c48f80fa744fcbd51
Stefan Dragoi <Stefan.Dragoi@centric.eu>
2024-07-16 21:33:52 +0300

    Implemented tests in RegisterRequestModelValidatorTests

--- SummerPractice.TravelPlanner/TravelPlanner.Validators.Tests/RegisterRequestModelValidatorTests.cs
+++ SummerPractice.TravelPlanner/TravelPlanner.Validators.Tests/RegisterRequestModelValidatorTests.cs
@@ -9,7 +9,7 @@ namespace TravelPlanner.Validators.Tests
         private readonly RegisterRequestModelValidator _validator = new();
 
         [Fact]
-        public void Should_HaveError_When_UsernameIsEmpty()
+        public void Should_Have_Error_When_Username_Is_Empty()
         {
             // Arrange
             var model = this.GetValidModel();
@@ -20,6 +20,90 @@ namespace TravelPlanner.Validators.Tests
 
             // Assert
             result.ShouldHaveValidationErrorFor(x => x.Username);
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Username_Is_Short()
+        {
+            // Arrange
+            var model = this.GetValidModel();
+            model.Username = "abc";
+
+            // Act
+            var result = this._validator.TestValidate(model);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.Username);
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Username_Is_Long()
+        {
+            // Arrange
+            var model = this.GetValidModel();
+            model.Username = new string('a', 21);
+
+            // Act
+            var result = this._validator.TestValidate(model);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.Username);
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Username_Has_Special_Characters()
+        {
+            // Arrange
+            var model = this.GetValidModel();
+            model.Username = "abc@123";
+
+            // Act
+            var result = this._validator.TestValidate(model);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.Username);
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Password_Is_Empty()
+        {
+            // Arrange
+            var model = this.GetValidModel();
+            model.Password = string.Empty;
+
+            // Act
+            var result = this._validator.TestValidate(model);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.Password);
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Password_Is_Short()
+        {
+            // Arrange
+            var model = this.GetValidModel();
+            model.Password = "Pass1!";
+
+            // Act
+            var result = this._validator.TestValidate(model);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.Password);
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Password_Does_Not_Contain_Uppercase()
+        {
+            // Arrange
+            var model = this.GetValidModel();
+            model.Password = "password1!";
+
+            // Act
+            var result = this._validator.TestValidate(model);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.Password);
         }
 
         [Fact]
@@ -37,7 +121,49 @@ namespace TravelPlanner.Validators.Tests
         }
 
         [Fact]
-        public void Should_HaveError_When_EmailIsInvalid()
+        public void Should_Have_Error_When_Password_Does_Not_Contain_Number()
+        {
+            // Arrange
+            var model = this.GetValidModel();
+            model.Password = "Password!";
+
+            // Act
+            var result = this._validator.TestValidate(model);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.Password);
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Password_Does_Not_Contain_Special_Character()
+        {
+            // Arrange
+            var model = this.GetValidModel();
+            model.Password = "Password1";
+
+            // Act
+            var result = this._validator.TestValidate(model);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.Password);
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Email_Is_Empty()
+        {
+            // Arrange
+            var model = this.GetValidModel();
+            model.Email = string.Empty;
+
+            // Act
+            var result = this._validator.TestValidate(model);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.Email);
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Email_Is_Invalid()
         {
             // Arrange
             var model = this.GetValidModel();
@@ -48,6 +174,33 @@ namespace TravelPlanner.Validators.Tests
 
             // Assert
             result.ShouldHaveValidationErrorFor(x => x.Email);
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Email_Domain_Is_Not_Allowed()
+        {
+            // Arrange
+            var model = this.GetValidModel();
+            model.Email = "user@notallowed.com";
+
+            // Act
+            var result = this._validator.TestValidate(model);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.Email);
+        }
+
+        [Fact]
+        public void Should_Not_Have_Error_Whenmodel_Is_Valid()
+        {
+            // Arrange
+            var model = this.GetValidModel();
+
+            // Act
+            var result = this._validator.TestValidate(model);
+
+            // Assert
+            result.ShouldNotHaveAnyValidationErrors();
         }
 
         private RegisterRequestModel GetValidModel()
