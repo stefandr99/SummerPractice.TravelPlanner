0af712930e8889ba38f439a89d2f908fddcd0623
Stefan Dragoi <Stefan.Dragoi@centric.eu>
2024-07-20 20:25:01 +0300

    Fixed User service returnsnull test & file scope namespace

--- SummerPractice.TravelPlanner/TravelPlanner.Application.Tests/Services/UserServiceTests.cs
+++ SummerPractice.TravelPlanner/TravelPlanner.Application.Tests/Services/UserServiceTests.cs
@@ -1,72 +1,71 @@
-﻿namespace TravelPlanner.Application.Tests.Services
+﻿namespace TravelPlanner.Application.Tests.Services;
+
+using Application.Services;
+using Application.Services.Interfaces;
+using Domain.Entities;
+using Domain.Interfaces;
+using FluentAssertions;
+using NSubstitute;
+
+public class UserServiceTests
 {
-    using Application.Services;
-    using Application.Services.Interfaces;
-    using Domain.Entities;
-    using Domain.Interfaces;
-    using FluentAssertions;
-    using NSubstitute;
+    private readonly IUserRepository _userRepository;
+    private readonly IUserService _userService;
 
-    public class UserServiceTests
+    public UserServiceTests()
     {
-        private readonly IUserRepository _userRepository;
-        private readonly IUserService _userService;
+        this._userRepository = Substitute.For<IUserRepository>();
+        this._userService = new UserService(this._userRepository);
+    }
 
-        public UserServiceTests()
+    [Fact]
+    public async Task GetUserByIdAsync_Should_ReturnMappedUser_When_UserExists()
+    {
+        // Arrange
+        var userId = 1;
+        this._userRepository.GetUserByIdAsync(2).Returns(new User()
         {
-            this._userRepository = Substitute.For<IUserRepository>();
-            this._userService = new UserService(this._userRepository);
-        }
+            Id = 2,
+            Username = "user123",
+            Email = "test@centric.eu"
+        });
 
-        [Fact]
-        public async Task GetUserByIdAsync_Should_ReturnMappedUser_When_UserExists()
-        {
-            // Arrange
-            var userId = 1;
-            this._userRepository.GetUserByIdAsync(2).Returns(new User()
-            {
-                Id = 2,
-                Username = "user123",
-                Email = "test@centric.eu"
-            });
+        // Act
+        var result = await this._userService.GetUserByIdAsync(userId);
 
-            // Act
-            var result = await this._userService.GetUserByIdAsync(userId);
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Data.Id.Should().Be(1);
+    }
 
-            // Assert
-            result.IsSuccess.Should().BeTrue();
-            result.Data.Id.Should().Be(1);
-        }
+    [Fact]
+    public async Task GetUserByIdAsync_Should_CallRepositoryMethodOnce_When_GetUserById()
+    {
+        // Arrange
+        var userId = 1;
 
-        [Fact]
-        public async Task GetUserByIdAsync_Should_CallRepositoryMethodOnce_When_GetUserById()
-        {
-            // Arrange
-            var userId = 1;
+        // Act
+        await this._userService.GetUserByIdAsync(userId);
 
-            // Act
-            await this._userService.GetUserByIdAsync(userId);
+        // Assert
+        await this._userRepository.Received(1).GetUserByIdAsync(userId);
+        await this._userRepository.Received(0).GetUserByUsernameAsync(Arg.Any<string>());
+    }
 
-            // Assert
-            await this._userRepository.Received(1).GetUserByIdAsync(userId);
-            await this._userRepository.Received(0).GetUserByUsernameAsync(Arg.Any<string>());
-        }
+    [Fact]
+    public async Task RegisterAsync_Should_ThrowException_When_UserWithThatUsernameAlreadyExists()
+    {
+        // Arrange
+        var username = "user123";
+        var password = "123456";
+        var email = "test123@centric.eu";
+        this._userRepository.GetUserByUsernameAsync(username).Returns(new User());
 
-        [Fact]
-        public async Task RegisterAsync_Should_ThrowException_When_UserWithThatUsernameAlreadyExists()
-        {
-            // Arrange
-            var username = "user123";
-            var password = "123456";
-            var email = "test123@centric.eu";
-            this._userRepository.GetUserByUsernameAsync(username).Returns(new User());
+        // Act
+        Func<Task> act = async () => await this._userService.RegisterAsync(username, password, email);
 
-            // Act
-            Func<Task> act = async () => await this._userService.RegisterAsync(username, password, email);
-
-            // Assert
-            await act.Should().ThrowAsync<ArgumentException>()
-                .WithMessage("Username already exists");
-        }
+        // Assert
+        await act.Should().ThrowAsync<ArgumentException>()
+            .WithMessage("Username already exists");
     }
 }
